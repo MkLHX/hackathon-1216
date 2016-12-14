56c7f3e8f58e30a8b4db3ee56445e503a04c114a
juliengrima <gogrinformatique@gmail.com>
2016-12-15 00:23:47 +0100

    debut upload

--- src/GoFlashBundle/Form/JeuType.php
+++ src/GoFlashBundle/Form/JeuType.php
@@ -5,6 +5,7 @@ namespace GoFlashBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class JeuType extends AbstractType
 {
@@ -13,7 +14,11 @@ class JeuType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('titre')->add('description')->add('imageObjectif')        ;
+        $builder
+            ->add('titre')
+            ->add('description')
+            ->add('imageObjectif', FileType::class, array('label' => 'Capturer'))
+        ;
     }
     
     /**
